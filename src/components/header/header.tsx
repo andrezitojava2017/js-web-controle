c8696a8b99725ca17bd36808b77518c117956b19
Jederson Andre <andre_sjx@live.com>
2023-10-25 11:04:06 -0300

    :lipstick:feat: implementado menu do componenteheader

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,11 +8,43 @@ const Header = () => {
         <span className="subtitle">Tec. em Desenvolvimento</span>
       </div>
       <div className="container-menu">
+        <div className="dropdown">
+          <span className="menu">Solicitação</span>
+          <div className="dropdown-content">
+            <p>Nova solicitação</p>
+            <p>Listar</p>
+            <p>Cad. Secretaria</p>
+            <p>Cad. Departamento</p>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <span className="menu">Movimentos</span>
+          <div className="dropdown-content">
+            <p>Entrada de Produto</p>
+            <p>Saida de Produto</p>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <span className="menu">Produtos</span>
+          <div className="dropdown-content">
+            <p >Novo Produto</p>
+            <p >Listar Produtos</p>
+          </div>
+        </div>
+        {/*
         <ul>
-          <li>Solicitação</li>
+          <li>Solicitação
+            <div className="dropdown">
+              <span>elemtno</span><br></br>
+              <span>elemtno</span>
+            </div>
+          </li>
           <li>Movimentos</li>
           <li>Produtos</li>
         </ul>
+  */}
       </div>
       <div>
         <span className="name-user">Jederson Andre</span>
