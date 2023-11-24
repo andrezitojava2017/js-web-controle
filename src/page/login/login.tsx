7349ffe397020e5604b0371fffe1b4ff6d47d463
Jederson Andre <andre_sjx@live.com>
2023-11-24 10:57:41 -0300

    :sparkles: feat: implementado logica para exibição de mensagens com componente snackbar

--- src/page/login/login.tsx
+++ src/page/login/login.tsx
@@ -4,24 +4,26 @@ import imageLogin from "../../assets/login.png";
 import ButtonJs from "../../components/button/button";
 import InputJs from "../../components/input/input";
 import { signInUser } from "../../services/auth";
-import {validateCredentials} from './actions/actions'
+import { validateCredentials } from "./actions/actions";
 import "./style/login.css";
+import MessageSnack from "../../components/messageSnack/messageSnack";
 
 const Login = () => {
-
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-
+  const [openSnack, setOpenSnack] = useState<boolean>(false);
+  const [messageError, setMessageError] = useState<string>("");
 
   const logar = async (): Promise<void> => {
-
     try {
       validateCredentials(email, password);
-      console.log('dados validos!!')
-    } catch (error) {
-      console.log(error)
+      console.log("dados validos!!");
+      //await signInUser(email, password);
+    } catch (error:any) {
+      setMessageError(error.message)
+      setOpenSnack(true)
     }
-    //await signInUser("andre_sjx@live.com", "@agc171213");
+    //
   };
 
   const sessao = async (): Promise<any> => {
@@ -35,8 +37,20 @@ const Login = () => {
         <img src={imageLogin} />
       </div>
       <div className="form">
-        <InputJs placeholder="Seu e-mail" type="email" name="email" setState={setEmail} value={email}/>
-        <InputJs placeholder="Password" type="password" name="password" setState={setPassword} value={password} />
+        <InputJs
+          placeholder="Seu e-mail"
+          type="email"
+          name="email"
+          setState={setEmail}
+          value={email}
+        />
+        <InputJs
+          placeholder="Password"
+          type="password"
+          name="password"
+          setState={setPassword}
+          value={password}
+        />
 
         <div style={{ paddingBottom: "4rem", paddingLeft: "8rem" }}>
           <a href="#">
@@ -47,6 +61,8 @@ const Login = () => {
         <ButtonJs value={"Logar"} class={"btn"} onClick={logar} />
         <ButtonJs value={"sessão"} class={"btn"} onClick={sessao} />
       </div>
+
+      <MessageSnack message={messageError} open={openSnack} type="error" onClose={()=>setOpenSnack(false)}/>
     </div>
   );
 };
