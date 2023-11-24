66f4941b8cf42bcb2ae48d497ce44f94c314f6d6
Jederson Andre <andre_sjx@live.com>
2023-11-24 16:04:28 -0300

    :sparkles: feat: implementado logica de autenticação e inclusao de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react";
 import Container from "./components/container/container";
+import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
-import Departament from "./page/departament/departament";
-import Home from "./page/home/home";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/route";
+import { supabase } from "./api/supabase";
 import Login from "./page/login/login";
-import Order from "./page/order/order";
-import Secretariat from "./page/secretariat/secretariat";
 
 function App() {
-  return (
-    <main>
-      <Header/>
-      <Container>
-        <Departament/>
-      </Container>
-    </main>
-  );
+  const [userLogin, setUserLogin] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (data.session != null) {
+        setUserLogin(true);
+      }
+    })();
+  }, []);
+
+ 
+  {
+    if (userLogin) {
+      return (
+        <main className="main">
+          <Header />
+          <Container>
+            <RouterProvider router={router} />
+          </Container>
+          <Footer />
+        </main>
+      );
+    }
+    return <Login />;
+  }
+
 }
 
 export default App;
