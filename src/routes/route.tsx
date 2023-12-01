9b66c862f8776a17194484d00188dcb3a9aa68f0
Jederson Andre <andre_sjx@live.com>
2023-12-01 16:37:01 -0300

    :recycle: refactor: removendo comentarios antigos

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "../page/login/login";
 import Departament from "../page/departament/departament";
 import Request from "../page/request/request";
@@ -6,23 +6,15 @@ import Order from "../page/order/order";
 import Secretariat from "../page/secretariat/secretariat";
 import Home from "../page/home/home";
 
-
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "new/departament",
-    element: <Departament />,
-  },
-  {
-    path: "new/secretaria",
-    element: <Secretariat />,
-  },
-  {
-    path: "order",
-    element: <Order />,
-  },
-]);
+export const RouteJs = () => {
+  return (
+    <Routes>
+    <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/novo/departamento" element={<Departament />} />
+      <Route path="/novo/secretaria" element={<Secretariat />} />
+      <Route path="/novo/solicitacao" element={<Request />} />
+      <Route path="/order" element={<Order />} />
+    </Routes>
+  );
+};
