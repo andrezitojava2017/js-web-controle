600120e34356aa8184383c12f09c5a46c47b3c9b
Jederson Andre <andre_sjx@live.com>
2023-12-04 16:58:18 -0300

    :recycle: refactor: inclusao do type date e disable no input

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -2,16 +2,16 @@ import "./style/input.css";
 
 type Props = {
   label?: string;
-  placeholder: string;
-  type: "email" | "text" | "password" | "number" | "search";
+  placeholder?: string;
+  type: "email" | "text" | "password" | "number" | "search" |"date";
   name: string;
   class?: string;
   style?: {};
+  disable?: boolean;
   //setState: React.Dispatch<React.SetStateAction<Credentials>>;
   value: string | number;
   setState: React.Dispatch<React.SetStateAction<string | object | any>>;
 };
-
 
 const InputJs = (props: Props) => {
   return (
@@ -20,13 +20,14 @@ const InputJs = (props: Props) => {
         <label>{props.label}</label>
       </div>
       <input
+        disabled={props.disable}
         placeholder={props.placeholder}
         type={props.type}
         name={props.name}
         className={props.class}
         style={{ ...props.style }}
         value={props.value}
-        onChange={(e)=> props.setState(e.target.value)}
+        onChange={(e) => props.setState(e.target.value)}
       />
     </>
   );
