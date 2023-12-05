8e9211e19c55d72cbb570c6001e9722978969ead
Jederson Andre <andre_sjx@live.com>
2023-12-05 11:27:40 -0300

    :sparkles: feat: implementado logica da pagina de cadastro de secretaria

--- src/page/secretariat/secretariat.tsx
+++ src/page/secretariat/secretariat.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import ButtonJs from "../../components/button/button";
 import InputJs from "../../components/input/input";
 import "./style/secretariat.css";
+import { addSeretariat } from "../../services/secretariat";
+import MessageSnack from "../../components/messageSnack/messageSnack";
 
 const Secretariat = () => {
+  const [secretaria, setSecretaria] = useState<string>("");
+  const [openSnack, setOpenSnack] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+
+  const newSecretariat = async () => {
+    try {
+      const rs = await addSeretariat(secretaria);
+      if (rs) {
+        setMessage(rs);
+        setOpenSnack(true);
+      }
+    } catch (error: any) {
+      setMessage(error);
+      setOpenSnack(true);
+    }
+  };
+
   return (
     <div className="content">
       <h3 style={{ textAlign: "center", padding: "0.8rem", color: "#575353" }}>
@@ -11,11 +31,13 @@ const Secretariat = () => {
       <div className="formulario">
         <div>
           <InputJs
+            value={secretaria}
+            setState={setSecretaria}
             label="Descrição"
             name="secretaria"
             type="text"
             placeholder="Digite nome da secretaria"
-            style={{width: "30em" }}
+            style={{ width: "30em" }}
           />
         </div>
       </div>
@@ -24,8 +46,15 @@ const Secretariat = () => {
           value="Salvar"
           class={"btn"}
           style={{ backgroundColor: "#1E1D59" }}
+          onClick={newSecretariat}
         />
       </div>
+      <MessageSnack
+        message={message}
+        open={openSnack}
+        type="success"
+        onClose={() => setOpenSnack(false)}
+      />
     </div>
   );
 };
