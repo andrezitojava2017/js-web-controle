1d80204ca84ccd2414cbf64258fea4bc949ea3e9
Jederson Andre <andre_sjx@live.com>
2023-12-07 11:38:05 -0300

    :sparkles: feat: implementado service para Products

--- src/services/products.tsx
+++ src/services/products.tsx
@@ -1,19 +1,32 @@
 import { supabase } from "../api/supabase";
 
-export const addProduct = async(product: string) => {
+export const addProduct = async (product: string) => {
   try {
     const { data, error } = await supabase
       .from("produtos")
       .insert([{ descricao: product }])
       .select();
 
-      if(error){
-        throw new Error("Ocorreu um erro inesperado na inserção do produto!")
-      }
+    if (error) {
+      throw new Error("Ocorreu um erro inesperado na inserção do produto!");
+    }
 
-     return data;
-     
+    return data;
   } catch (error) {
-    throw error
+    throw error;
+  }
+};
+
+export const allProducts = async () => {
+  try {
+    const { data, error } = await supabase.from("produtos").select('*');
+
+    if (error) {
+      throw new Error("Ocorreu um erro inesperado na busca do produto!");
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
   }
 };
