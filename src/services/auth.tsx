e17915182e1e485dd0a76c046ac98e79bbc82800
Jederson Andre <andre_sjx@live.com>
2023-12-07 15:04:37 -0300

    :sparkles: feat: implementado funções que recupera dados de uma sessão logada e função para inserir metadados de um usuario logado

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -10,7 +10,43 @@ export const signInUser = async (
   });
 
   if (error) {
-    throw new Error(error.message)
+    throw new Error(error.message);
   }
   return data;
+};
+
+export const updateDataUser = async () => {
+  try {
+    const { data, error } = await supabase.auth.updateUser({
+      data: {
+        nome: "Jederson @ndré",
+        secretaria: "2",
+        cargo: "Supervisor de Tecnologia",
+      },
+    });
+    if (error) {
+      throw new Error("Ocorreu um erro inesperado na atualização do usuario!!");
+    }
+
+    console.log(data);
+  } catch (error: any) {
+    console.log(error.message);
+    throw error;
+  }
+};
+
+export const infoSession = async () => {
+  try {
+    const { data, error } = await supabase.auth.getSession();
+    if (error) {
+      throw new Error(
+        "Ocorreu um erro inesperado na tentativa de recuperar dados da sessão!"
+      );
+    }
+    return data.session?.user;
+    
+  } catch (error: any) {
+    console.error(error.message);
+    throw error;
+  }
 };
