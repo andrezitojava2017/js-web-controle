cb286490ca8da439faf24c58ca6d22d4d54c70bf
Jederson Andre <andre_sjx@live.com>
2023-12-19 11:06:21 -0300

    :sparkles: feat: inclusao de serviços supabase para saida da sessão e atualização de dados do usuario logado

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,20 +1,30 @@
 import { supabase } from "../../api/supabase";
+import { updateDataUser } from "../../services/auth";
+import "./style/home.css";
 
 const Home = () => {
-  const signOut = async ()=> {
+  const signOut = async () => {
     const { error } = await supabase.auth.signOut();
-    console.log('Usuario deslogado')
-  }
+    console.log("Usuario deslogado");
+    window.location.reload();
+  };
 
-  const sessao = async ()=>{
+  const sessao = async () => {
     const { data, error } = await supabase.auth.getSession();
-    console.log(data)
-  }
+    console.log(data);
+  };
+
+  const atualizarDados = async () => {
+    await updateDataUser();
+  };
+
   return (
-    <div style={{ background: "blue" }}>
+    <div className="home">
       <h3>Bem vindo ao Web Controle</h3>
       <input type="button" value="SAIR" onClick={signOut} />
       <input type="button" value="SESSÃO" onClick={sessao} />
+
+      <input type="button" value="ATUALIZAR" onClick={atualizarDados} />
     </div>
   );
 };
